50f24cd09431bf4127ef2273ea3a268ec35ed591
ashishghost9821-design <ashishghost9821@gmail.com>
2026-09-14 20:44:29 +0530

    Update project02.c

--- project02.c
+++ project02.c
@@ -4,7 +4,6 @@
 /*
  * ╔══════════════════════════════════════════════╗
  *           C PROGRAMMING - MAD LIBS GAME
- *           Bro Code Tutorial | My Notes
  * ╚══════════════════════════════════════════════╝
  *
  * Concepts practiced:
